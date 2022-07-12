1d8cfd6eecde31d14eb76edf2b9364d711fabc5d
franciscodito <franciscodito@gmail.com>
2022-07-12 06:40:57 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('node:path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+const app = express();
+
+//Importing routes
+const customerRoutes = require('./routes/customer');
+const { urlencoded } = require('express');
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Middlewares
+app.use(morgan('dev'));
+
+app.use(myConnection(mysql, {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        port: 3306,
+        database: 'crudnodejs'
+    }, 'single'));
+
+app.use(express.urlencoded({
+        extended: false
+}));    
+
+//Routes
+app.use('/', customerRoutes);
+
+//Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Listening server
+app.listen(app.get('port'), () => {
+    console.log('Server on port 3000');
+})
